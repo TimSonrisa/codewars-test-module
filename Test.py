74dce065b3c0641b2290505d3113ec0c82321cde
TimSonrisa <33624906+TimSonrisa@users.noreply.github.com>
2019-07-27 12:56:54 +0300

    Test Class

---
+++ Test.py
@@ -0,0 +1,20 @@
+class Test:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def describe(message):
+        print(message)
+
+    @staticmethod
+    def it(message):
+        print(message)
+
+    @staticmethod
+    def assert_equals(actual, expected):
+        if actual == expected:
+            print("\tTest Passed!")
+        else:
+            print("\tTest Failed:")
+            print(f"\t'{actual}' should equal '{expected}'")
